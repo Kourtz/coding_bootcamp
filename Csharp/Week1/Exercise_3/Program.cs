a2bcd949bfeafcb2ba5b4230f13de0ae1c2f552f
Kourtz <christos.kourtz@gmail.com>
2016-10-19 22:51:23 +0300

    ex3

---
+++ Csharp/Week1/Exercise_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        public static class Utilities
+        {
+            //einai static giati einai san math px
+            public static void Swap<T>(ref T a,ref T b)
+            {
+                T temp = a;
+                a =b;
+                b =temp;
+            }
+        }
+
+        static void Main(string[] args)
+        {        
+            int a = 5;
+            int b = 10;
+            Utilities.Swap(ref a, ref b);
+
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
